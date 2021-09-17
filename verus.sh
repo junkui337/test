50a9c0ed9749e764735d3816458226dfc20b1d5c
junkui337 <90859327+junkui337@users.noreply.github.com>
2021-09-17 14:14:34 +0000

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RUTqEnbHECniDfNJhqfoMpwP8v5Za6U56V.iful61 -p x --cpu 7
